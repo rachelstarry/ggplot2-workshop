54b51a676cc7e2200fdee9c926663d0a6a6a9446
rachelstarry <rachel.starry@gmail.com>
2019-10-10 10:49:37 -0400

    Created R script

---
+++ ggplot2Workshop.R
@@ -0,0 +1,7 @@
+# Install packages (only run these lines ONCE; erase the # to "uncomment" this line and run it)
+# install.packages("tidyverse")
+
+# Load the required packages into our working environment
+library(tidyverse)
+
+# 
